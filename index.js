9661b4944e8cfc43dc02d25d4818585313ed7233
Aleksandr Kanunnikov <lifeart92@gmail.com>
2020-08-05 17:46:52 +0300

    add ast transofrm for default case

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 'use strict';
 
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+  setupPreprocessorRegistry(type, registry) {
+    const plugin = this._buildPlugin();
+
+    plugin.parallelBabel = {
+      requireFile: __filename,
+      buildUsing: '_buildPlugin',
+      params: {}
+    };
+
+    registry.add('htmlbars-ast-plugin', plugin);
+  },
+
+  _buildPlugin() {
+    const RefTransform = require('./lib/ref-transform');
+
+    return {
+      name: 'ref-transform',
+      plugin: RefTransform,
+      baseDir() {
+        return __dirname;
+      }
+    };
+  }
 };
